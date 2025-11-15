3dd86d765e5b98d31a17a26165e26cf5338e3559
Le Van Loc <24520982@gm.uit.edu.vn>
2025-11-15 20:44:59 +0700

    lvl upcode

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <cstdlib>
+#include <ctime>
 #include <conio.h>
 using namespace std;
 
@@ -12,62 +13,96 @@ struct Point {
 
 class CONRAN {
 public:
-    Point A[100];
+    Point A[200];
     int DoDai;
 
     CONRAN() {
         DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
+        A[0].x = 12; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        A[2].x = 10; A[2].y = 10;
     }
-
+	
     void Ve() {
-        for (int i = 0; i < DoDai; i++) {
+    	gotoxy(A[0].x, A[0].y); 
+		cout << "x";
+		
+        for (int i = 1; i < DoDai; i++) {
             gotoxy(A[i].x, A[i].y);
-            cout << "O";
+            cout << "o";
         }
     }
-
+	
     void DiChuyen(int Huong) {
         for (int i = DoDai - 1; i > 0; i--)
             A[i] = A[i - 1];
-        if (Huong == 0) A[0].x = A[0].x + 1; 
+
+        if (Huong == 0) A[0].x = A[0].x + 1;
         if (Huong == 1) A[0].y = A[0].y + 1;
         if (Huong == 2) A[0].x = A[0].x - 1; 
-        if (Huong == 3) A[0].y = A[0].y - 1; 
+        if (Huong == 3) A[0].y = A[0].y - 1;
+    }
+	
+    bool KiemTraAnMoi(Point moi) {
+        return (A[0].x == moi.x && A[0].y == moi.y);
+    }
+	
+    void TangDoDai() {
+        A[DoDai] = A[DoDai - 1];
+        DoDai++;
     }
 };
 
-// Vẽ khung (width x height)
 void VeKhung(int width, int height) {
     for (int x = 0; x <= width; x++) {
-        gotoxy(x, 0); cout << "#";              
-        gotoxy(x, height); cout << "#";        
+        gotoxy(x, 0); cout << "#";	
+        gotoxy(x, height); cout << "#"; 
     }
     for (int y = 0; y <= height; y++) {
-        gotoxy(0, y); cout << "#";              
-        gotoxy(width, y); cout << "#";         
+        gotoxy(0, y); cout << "#";      
+        gotoxy(width, y); cout << "#";   
     }
 }
 
+void VeMoi(Point moi) {
+    gotoxy(moi.x, moi.y);
+    cout << "*";
+}
+
 int main() {
+    srand(time(NULL));
+
     CONRAN r;
     int Huong = 0;
     char t;
     const int width = 50, height = 20;
+    Point moi;
+
+    moi.x = rand() % (width - 2) + 1;
+    moi.y = rand() % (height - 2) + 1;
 
     while (1) {
         if (kbhit()) {
             t = getch();
-            if (t == 'a') Huong = 2;
-            if (t == 'w') Huong = 3;
-            if (t == 'd') Huong = 0;
-            if (t == 's') Huong = 1;
+            if (t == 'a' && Huong != 0) Huong = 2;
+            if (t == 'w' && Huong != 1) Huong = 3;
+            if (t == 'd' && Huong != 2) Huong = 0;
+            if (t == 's' && Huong != 3) Huong = 1;
         }
-        system("cls"); 
+
+        system("cls");
+
         VeKhung(width, height);
+        VeMoi(moi);
+
         r.Ve();
+
+        if (r.KiemTraAnMoi(moi)) {
+            r.TangDoDai();
+            moi.x = rand() % (width - 2) + 1;
+            moi.y = rand() % (height - 2) + 1;
+        }
+
         r.DiChuyen(Huong);
 
         if (r.A[0].x <= 0 || r.A[0].x >= width || r.A[0].y <= 0 || r.A[0].y >= height) {
@@ -75,11 +110,24 @@ int main() {
             cout << "GAME OVER!";
             break;
         }
+        
+        for (int i = 1; i < r.DoDai; i++) {
+            if (r.A[0].x == r.A[i].x && r.A[0].y == r.A[i].y) {
+                gotoxy(width / 2 - 5, height / 2);
+                cout << "GAME OVER!";
+                return 0;
+            }
+        }
+        
+        
+
         Sleep(150);
     }
+
     gotoxy(0, height + 2);
     return 0;
 }
+
 void gotoxy(int column, int line) {
     COORD coord;
     coord.X = column;
